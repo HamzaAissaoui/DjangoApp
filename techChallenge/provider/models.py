ed57a26565c3458c99bf9161d6cb82ac79d2f098
hamzaaissaoui <hamzamiller5@gmail.com>
2022-05-16 16:12:58 +0100

    postgres configuration

--- techChallenge/provider/models.py
+++ techChallenge/provider/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class Provider(models.Model):
+    pass
--- techChallenge/techChallenge/urls.py
+++ techChallenge/techChallenge/urls.py
@@ -18,8 +18,9 @@ from django.urls import path
 from django.conf.urls import include
 
 urlpatterns = [
-    path('api/v1/', include([
-        path('provider/', include('provider.urls')),
+    path('api/v1/', 
+        include([
+            path('provider/', include('provider.urls')),
         ])
     ),
     path('admin/', admin.site.urls),
